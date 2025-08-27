50e211d1fd45bd7cc4fe015a84370b804cd74c1b
najdza <witamdzwoniewsprawiekukurydzy@gmail.com>
2025-08-27 17:45:09 +0200

    dodanie fish do folderu na wypadek wielkiego rozpierodlu

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+# Created by `pipx` on 2023-10-13 17:54:22
+set PATH $PATH /home/can/.local/bin
+set PATH $PATH /home/can/.cargo/bin
+set PATH $PATH /home/can/.config/emacs/bin/
---
+++ .config/fish/functions/bigtime.fish
@@ -0,0 +1,3 @@
+function bigtime --description 'alias bigtime=bigtime --file simple -sdfi -F 2'
+    command bigtime --file simple -sdfi -F 2 $argv
+end
