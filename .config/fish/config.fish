55e5d56944ac7ec0fe21eb24740385a2ce3dfffb
najdza <witamdzwoniewsprawiekukurydzy@gmail.com>
2025-08-30 22:42:35 +0200

    cwel

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-
+    # neofetch
     # aliasy
     alias hh='helix'
+    alias pobierz='sudo pacman -S'
 end
 # Created by `pipx` on 2023-10-13 17:54:22
 set PATH $PATH /home/can/.local/bin
 set PATH $PATH /home/can/.cargo/bin
 set PATH $PATH /home/can/.config/emacs/bin/
 # alias
+# name: 'Cool Beans'
+# preferred_background: black
